c3ba16b0858a8a9da46ba5d0dc5c3b03452ea4ce
Shubhankar Das <das.shubhankar97@gmail.com>
2020-09-08 21:29:05 +0530

    completed login form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -12,31 +12,26 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
   validate = () => {
-    const errors = {};
     const { account } = this.state;
-    const result = Joi.validate(account, this.schema, { abortEarly: false });
-    console.log(result);
+    const option = { abortEarly: false };
+    const { error } = Joi.validate(account, this.schema, option);
+    if (!error) return null;
 
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? {} : errors;
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const {error} = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleChange = ({ currentTarget: input }) => {
@@ -75,18 +70,20 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
-            errors={errors.username}
+            errors={errors ? errors.username : null}
           />
-          
+
           <Input
             name="password"
             value={account.password}
             label="Password"
             type="password"
             onChange={this.handleChange}
-            errors={errors.password}
+            errors={errors ? errors.password : null}
           />
-          <button className="btn btn-primary">Login</button>
+          <button
+          disabled={this.validate()} 
+          className="btn btn-primary">Login</button>
         </form>
       </div>
     );
