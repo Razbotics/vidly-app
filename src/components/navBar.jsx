072f9459dabea65a538dad63c8f8f015876c0aa7
Shubhankar Das <das.shubhankar97@gmail.com>
2020-09-14 23:03:18 +0530

    changed to react-bootstrap navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,61 +1,37 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
 import Dropdown from "./common/dropdown";
-import auth from "../services/authService"
+import auth from "../services/authService";
+import { Nav, Navbar } from "react-bootstrap";
 
 const NavBar = () => {
   const user = auth.getCurrentUser();
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" className="navbar-brand">
-        Vidly
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="true"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          {!user && (
-            <React.Fragment>
-              <NavLink className="nav-link" to="/login">
-                Login
-              </NavLink>
-              <NavLink className="nav-link" to="/register">
-                Register
-              </NavLink>
-            </React.Fragment>
-          )}
-          {user && (
-            <React.Fragment>
-              <NavLink className="nav-link" to="/movies">
-                Movies
-              </NavLink>
-              <NavLink className="nav-link" to="/customers">
-                Customers
-              </NavLink>
-              <NavLink className="nav-link" to="/rentals">
-                Rentals
-              </NavLink>
-              <Dropdown
-                label={user.name.split(" ")[0]}
-                dropdowns={[
-                  { label: "Profile", link: "/profile" },
-                  { label: "Logout", link: "/logout" },
-                ]}
-              />
-            </React.Fragment>
-          )}
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Vidly</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        {!user && (
+          <Nav className="mr-auto">
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/logout">Logout</Nav.Link>
+          </Nav>
+        )}
+        {user && (
+          <Nav className="mr-auto">
+            <Nav.Link href="/movies">Movies</Nav.Link>
+            <Nav.Link href="/customers">Customers</Nav.Link>
+            <Nav.Link href="/rentals">Rentals</Nav.Link>
+            <Dropdown
+              label={user.name.split(" ")[0]}
+              dropdowns={[
+                { label: "Profile", link: "/profile" },
+                { label: "Logout", link: "/logout" },
+              ]}
+            />
+          </Nav>
+        )}
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
